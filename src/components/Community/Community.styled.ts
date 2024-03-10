7676e0da08f439242ca8a470bab7693a7e6625fc
Taras <taras.papka@protonmail.com>
2024-03-10 22:56:10 +0200

    added styles for tablet and desktop, corrected styles for btn

--- src/components/Community/Community.styled.ts
+++ src/components/Community/Community.styled.ts
@@ -2,19 +2,68 @@ import { styled } from "styled-components";
 import { colors } from "../../styles/colors";
 import { breakpoint } from "../../styles/breakpoints";
 
+
 export const CommunitySection = styled.section`
 width: 100%;
 height: auto;
 background-color:  ${colors.menuLayoutColor};
 position: relative;
 outline: 1px solid red;
+
 `
 export const CommunityTitle = styled.h2`
 display: none;
 `
+export const DecoImg = styled.img`
+width: 54px;
+height: 54px;
+position: absolute;
+top: 27px;
+right: 0px;
+@media only screen and (min-width: ${breakpoint.secondMobile}) {
+top: 33px;
+right: 31px;
+}
+@media only screen and (min-width: ${breakpoint.tablet}) {
+  width: 114px;
+  height: 114px;
+top: 30px;
+right: 56px;
+}
+@media only screen and (min-width: ${breakpoint.desktop}) {
+  width: 200px;
+  height: 200px;
+top: 30px;
+right: 60px;
+}
+`
 
+export const DecoImgSecond = styled.img`
+width: 54px;
+height: 54px;
+position: absolute;
+bottom: 21px;
+left: 0px;
+@media only screen and (min-width: ${breakpoint.secondMobile}) {
+bottom: 9px;
+left: 7px;
+}
+@media only screen and (min-width: ${breakpoint.tablet}) {
+  width: 114px;
+  height: 114px;
+bottom: 36px;
+left: -20px;
+}
+@media only screen and (min-width: ${breakpoint.desktop}) {
+  width: 200px;
+  height: 200px;
+bottom: 57px;
+left: 41px;
+}
+`
 export const CommunityContainer = styled.div`
-padding: 115px 15px 123px 15px;
+padding-top: 115px;
+padding-bottom: 123px;
 /* text-align: center; */
 max-width: 344px;
 margin-left: auto;
@@ -22,11 +71,21 @@ margin-right: auto;
 z-index: 1;
 @media only screen and (min-width: ${breakpoint.tablet}) {
   min-width: 474px;
+  padding: 185px 0px;
+  /* padding-bottom: 0; */
+  > button{
+    font-size: 24px;
+  }
   }
   @media only screen and (min-width: ${breakpoint.desktop}) {
-    display: flex;
-    align-items: center;
-    max-width: 1440px;
+    /* display: flex;
+    align-items: center; */
+    min-width: 542px;
+     padding: 90px 0px;
+       > button{
+   margin-left: 0;
+   padding: 19px 115px;
+  }
   }
 `
 
--- src/components/Community/Community.tsx
+++ src/components/Community/Community.tsx
@@ -1,11 +1,14 @@
 import ButtonMain from '../../UI/Button/Button';
 import * as Styled from './Community.styled';
 
+
 export const Community = () => {
 
     return (
         <Styled.CommunitySection>
             <Styled.CommunityTitle>Спільнота</Styled.CommunityTitle>
+            <Styled.DecoImg src='/star.png'></Styled.DecoImg>
+            <Styled.DecoImgSecond src='/star.png'></Styled.DecoImgSecond>
             <Styled.CommunityContainer>
                 <Styled.CommunityText>
                     Бажаєш приєднатися до команди майстрів у нашому ком’юніті? 
